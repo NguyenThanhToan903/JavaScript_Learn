28ffbe34c714b8137c1d45bff2b21ab5c38a0e17
Nguyen Toan <codehub.personal@gmail.com>
2023-12-09 23:39:09 +0700

    finish TT SoSanh

--- js/main.js
+++ js/main.js
@@ -13,3 +13,24 @@ a = 5; // x <- y
 b = '5';
 
 console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a===b);
+//nen luon dung 3 dau bang (===)
+// === type, value.
+// == value.
+
+//Empty, null, undefined
+let c;
+//undefined
+console.log('check value: ',c)
+
+//Empty
+c = {};
+console.log('check value: ',c)
+
+//Null
+c = null;
+console.log('check value: ',c)
+
+//gan cho no cai gi thi no la cai do, khong can quan tam null, nen gan ''
+let d = '';
+d = 5;
+d = ['11'];
