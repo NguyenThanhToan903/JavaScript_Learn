f6677762260f4e7321629941614cff1337661801
Nguyen Toan <codehub.personal@gmail.com>
2023-12-09 22:44:41 +0700

    update Object and Arr

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 console.log("hello, world!");
 console.log(`hello`.toUpperCase());
 
@@ -43,4 +44,41 @@ console.log('Type of temp: ', temp);
  Han[a] = 'Toan';
 console.log('Ai, ', Han.name, '\nDia chi: ', Han.address);
 console.log('Ai, ', Han[a], '\nDia chi: ', Han['address']);
+*/
+
+//Array!!
+
+console.log("Hello, Array!");
+
+let a = {
+    name: 'Toan',
+    age: 20
+};
+
+console.log('check data object: ', a);
+console.log('my mane\'s: ', a.name);
+
+//arr = [item1, item2, item3];
+let b = ['Apple', 'SamSung', 'Nokia', 'Huawei'];
+console.log('typeof b: ', typeof b, b);
+console.log('First item: ', b[0], '\nSecond item: ', b[1]);
+// let i;
+// for(i = 0; i < b.length; i++){
+//     console.log(`item[${i}]: `, b[i]);
+// }
+
+let c = [
+    {
+        name: 'Toan',
+        age: 20
+    },
+    {
+        name: 'Han',
+        age: 19
+    },
+]
+
+for(i = 0; i < c.length; i++){
+    console.log(`item[${i}]: `, c[i]);
+}
 
