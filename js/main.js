0e9d0a9b3f9cb03e07ca6c3df63a30d1cb63ef71
Nguyen Toan <codehub.personal@gmail.com>
2023-12-09 23:25:43 +0700

    update TT SoSanh

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 console.log('Hello world from HTML');
 let a = 5; // x <- y
 let b = 5;
+
+console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a==b);
+
+a = 5; // x <- y
+b = '5';
+
+console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a==b);
+
+a = 5; // x <- y
+b = '5';
+
+console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a===b);
