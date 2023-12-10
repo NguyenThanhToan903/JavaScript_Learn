9b2d7ade44ef29a43fcfb6cce318f64d331dfc54
Nguyen Toan <codehub.personal@gmail.com>
2023-12-10 09:36:42 +0700

    Update Loop

--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
-console.log('Hello world from HTML');
-let a = 5; // x <- y
-let b = 5;
+// console.log('Hello world from HTML');
+// let a = 5; // x <- y
+// let b = 5;
 
-console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a==b);
+// console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a==b);
 
-a = 5; // x <- y
-b = '5';
+// a = 5; // x <- y
+// b = '5';
 
-console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a==b);
+// console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a==b);
 
-a = 5; // x <- y
-b = '5';
+// a = 5; // x <- y
+// b = '5';
 
-console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a===b);
-//nen luon dung 3 dau bang (===)
-// === type, value.
-// == value.
+// console.log('compare a and b: ', 'a = ', a, 'b = ', b, 'result: ', a===b);
+// //nen luon dung 3 dau bang (===)
+// // === type, value.
+// // == value.
 
-//Empty, null, undefined
-let c;
-//undefined
-console.log('check value: ',c)
+// //Empty, null, undefined
+// let c;
+// //undefined
+// console.log('check value: ',c)
 
-//Empty
-c = {};
-console.log('check value: ',c)
+// //Empty
+// c = {};
+// console.log('check value: ',c)
 
-//Null
-c = null;
-console.log('check value: ',c)
+// //Null
+// c = null;
+// console.log('check value: ',c)
 
-//gan cho no cai gi thi no la cai do, khong can quan tam null, nen gan ''
-let d = '';
-d = 5;
-d = ['11'];
+// //gan cho no cai gi thi no la cai do, khong can quan tam null, nen gan ''
+// let d = '';
+// d = 5;
+// d = ['11'];
+
+
+//Loop
+let i = 0;
+for(i = 1; i < 10; i++){
+    console.log(i);
+}
+
+console.log('check value: ',i);
+
+i = 0;
+for(let i = 1; i < 10; i++){
+    console.log(i);
+}
+
+console.log('check value: ',i);
