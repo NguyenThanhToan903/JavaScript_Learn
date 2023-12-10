849a969dfd6a330913cbba35cb4641cfb662f061
Nguyen Toan <codehub.personal@gmail.com>
2023-12-10 12:39:39 +0700

    update switch

--- js/main.js
+++ js/main.js
@@ -52,44 +52,19 @@
 // console.log('check value: ',i);
 
 
-// let arr = ['Apple', 'Samsung', 'Nokia', 'Huawei'];
-
-// // let i = 0;
-
-// // while(i < arr.length){
-// //     console.log('Hang: ', arr[i]);
-// //     i++;
-// // }
+let arr = ['Apple', 'Samsung', 'Nokia', 'Huawei'];
 
 // let i = 0;
+
 // while(i < arr.length){
-//     if(arr[i].length === 7){
-//         console.log('Hang co 7 chu: ', arr[i]);
-//     }
+//     console.log('Hang: ', arr[i]);
 //     i++;
 // }
 
-switch (new Date().getDay()) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-       day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-  }
-
-  console.log('Today is:', day);
+let i = 0;
+while(i < arr.length){
+    if(arr[i].length === 7){
+        console.log('Hang co 7 chu: ', arr[i]);
+    }
+    i++;
+}
